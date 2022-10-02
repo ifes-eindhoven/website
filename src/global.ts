45820c20f205d063112a669f3a684d93f188d159
Joan Marcè i Igual <jmigual@users.noreply.github.com>
2022-10-02 20:34:28 +0200

    Rewrite in Bootstrap

--- src/global.ts
+++ src/global.ts
@@ -3,7 +3,7 @@ import './css/global.scss';
 import 'bootstrap/js/dist/button';
 import 'bootstrap/js/dist/carousel';
 import 'bootstrap/js/dist/collapse';
-import 'bootstrap/js/dist/dropdown';
+// import 'bootstrap/js/dist/dropdown';
 
 import 'bootstrap-icons/font/bootstrap-icons.scss';
 // import 'bootstrap';
